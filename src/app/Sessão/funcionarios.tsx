97eda7092a588902ac2041080e2cfcfb1541726d
Rayner <raynerwhanyfek@gmail.com>
2025-10-14 11:40:24 -0300

    excluir uma linha do zod

--- src/app/Sessão/funcionarios.tsx
+++ src/app/Sessão/funcionarios.tsx
@@ -1,6 +1,4 @@
-import Team2
-from "@/components/mvpblocks/team-2";
-import { de } from "zod/v4/locales";
+import Team2 from "@/components/mvpblocks/team-2";
 
 export default function Funcionarios() {
   return (
